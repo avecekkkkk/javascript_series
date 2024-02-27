f0c961cc0e27ae9a5c1cb164ef6df5c936466d7a
avecekkkkk <160516409+avecekkkkk@users.noreply.github.com>
2024-02-27 09:18:25 +0000

    Variables & Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = "123485930"
+let accountmail = "avecek@gmail.com"
+var accountpassword = "password123"
+accountCity = "Birtamod"
+
+//accountId = "87942749"
+accountmail ="ram@gmail.com"
+accountpassword ="pass123"
+accountCity = "Birtabazar"
+
+let accountState;
+
+/*
+We dont use var because of issue with block scope and functional scope
+*/
+
+console.table([accountId, accountmail, accountpassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+//alert( 3 + 3) We are using nodejs,not browser
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value (represntation of empty value)
+//undefined => 
+//symbol => unque
+
+//object
+let name = "avecek"
+let age = 23
+let isLoggedIn = false
+let state;
+console.log(typeof "avecek");
